5f22e3d35929dc5a184f1b9422e9aac62d09c7fe
Ryan <ryanpaynt@uncc.edu>
2021-05-26 15:45:43 -0400

    restarted js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,24 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-console.log('hello');
 var formEl = document.querySelector('#searchCity');
 var city = document.querySelector('#city');
 var btn = document.querySelector('#submit');
+var ulEl - document.querySelector('')
 
 var lat;
 var long;
 var name;
+var arr;
+
+var weatherData = JSON.parse(localStorage.getItem("weatherData")) || [];
 
 var currentWeather = function (event) {
     event.preventDefault();
 
+
     var cityName = city.value;
+
     console.log(cityName);
     var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=5e18e2ae4a8234835170e48b2a9705f3'
     fetch(apiURL).then(function (response) {
@@ -29,7 +34,6 @@ var currentWeather = function (event) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
-                displayDayWeather(data);
             });
         } else {
             console.log('Error: ' + response.statusText);
@@ -41,57 +45,13 @@ var currentWeather = function (event) {
 
 };
 
-var fiveDayWeather = function (event) {
-    event.preventDefault();
-
-    var cityName = city.value;
-    var apiURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=metric&appid=5e18e2ae4a8234835170e48b2a9705f3'
-    fetch(apiURL).then(function (response) {
-        console.log(response.ok);
-        if (response.ok) {
-            return response.json();
-
-        } else {
-            console.log('Error: ' + response.statusText);
-        }
-    })
-        .then(function (data) {
-            console.log(data);
-        })
-        .catch(function (error) {
-            console.log('Unable to connect geocode');
-        })
-
-};
-
+var renderCity = function(){
+    function
+}
 
 formEl.addEventListener('submit', currentWeather);
-formEl.addEventListener('submit', fiveDayWeather);
-
-var displayDayWeather = function (weatherDay) {
-    if (weatherDay.length === 0) {
-        return;
-    }
-    console.log(weatherDay.main.temp);
-
-    var ulEl = document.querySelector('#dayWeather');
-    var headEl = document.querySelector('#weatherOverview');
-
-    var listEl1 = document.createElement('li');
-    var listEl2 = document.createElement('li');
-    var listEl3 = document.createElement('li');
-    var listEl4 = document.createElement('li');
-
-    var header = document.createElement('h2');
-    header.textContent = weatherDay.name + ' ' + moment().format('MM/Do');
-    headEl.appendChild(header);
-
-    listEl1.textContent = 'Current Temperature: ' + weatherDay.main.temp + '˚C';
-    ulEl.appendChild(listEl1);
-
-    listEl2.textContent = 'Current Wind Speed: ' + weatherDay.wind.speed + ' mph';
-    ulEl.appendChild(listEl2);
-
-    listEl3.textContent = 'Current Humidity: ' + weatherDay.main.humidity + '%';
-    ulEl.appendChild(listEl3);
-};
+btn.addEventListener('click', function(){
+    weatherData.push(city.value);
+    localStorage.setItem('weatherData', JSON.stringify(weatherData));
+    renderCity();
+});
